88959b537cb5a970cef50cc61901ad2ca5429ce6
aalexsd <alexduarteduartesilva@gmail.com>
2023-10-27 15:52:24 -0300

    onboarding screen

--- lib/widgets/auth_check.dart
+++ lib/widgets/auth_check.dart
@@ -1,10 +1,11 @@
-import 'package:Carrrabicho/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../Services/auth_services.dart';
+import '../screens/home_screen.dart';
 import '../screens/login_page.dart';
-
+import 'onboarding.dart'; // Importe sua página de OnBoardingPage ou AuthCheck
 
 class AuthCheck extends StatefulWidget {
   const AuthCheck({Key? key}) : super(key: key);
@@ -14,6 +15,32 @@ class AuthCheck extends StatefulWidget {
 }
 
 class _AuthCheckState extends State<AuthCheck> {
+  bool isFirstTimeUser = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkFirstTimeUser();
+  }
+
+  void checkFirstTimeUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isFirstTimeUser = prefs.getBool('isFirstTimeUser') ?? true;
+
+    if (isFirstTimeUser) {
+      // É a primeira vez do usuário, direcione-o para a página OnBoardingPage
+      prefs.setBool('isFirstTimeUser', false); // Marque que o usuário já não é mais de primeira vez
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => OnBoardingPage()), // Substitua 'OnBoardingPage' pela sua página real
+      );
+    } else {
+      // O usuário já acessou o aplicativo antes, então direcione-o para a tela de login.
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginPage()), // Substitua 'LoginPage' pela sua página real
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     AuthService auth = Provider.of<AuthService>(context);
---
+++ lib/widgets/onboarding.dart
@@ -0,0 +1,138 @@
+import 'package:Carrrabicho/screens/login_page.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import 'auth_check.dart';
+
+
+class OnBoardingPage extends StatefulWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  OnBoardingPageState createState() => OnBoardingPageState();
+}
+
+class OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => const LoginPage()),
+    );
+  }
+
+  Widget _buildFullscreenImage() {
+    return Image.asset(
+      'loginBG.png',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/images/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      allowImplicitScrolling: false,
+      autoScrollDuration: 3000,
+      infiniteAutoScroll: false,
+      // globalHeader: Align(
+      //   alignment: Alignment.topRight,
+      //   child: SafeArea(
+      //     child: Padding(
+      //       padding: const EdgeInsets.only(top: 16, right: 16),
+      //       child: _buildImage('logo.png', 100),
+      //     ),
+      //   ),
+      // ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.indigo
+          ),
+          child: const Text(
+            'Vamos lá!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Quem Somos",
+          body:
+          "Nós somos apaixonados por pets! Nosso aplicativo é o lugar onde os amantes de animais de estimação se encontram para encontrar os melhores serviços para seus companheiros peludos.",
+          image: _buildImage('logo.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Encontre o Melhor para Seu Pet",
+          body:
+          "Descubra serviços de qualidade para o seu pet com facilidade. Seja um veterinário atencioso, um cuidador carinhoso, um adestrador habilidoso ou um hotel que mimará seu amigo peludo.",
+          image: _buildImage('logo.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Cuide do Seu Melhor Amigo",
+          body:
+          "Nossa missão é ajudar você a manter seu pet saudável e feliz. Conecte-se com os melhores profissionais e ofereça o melhor cuidado para o seu pet.",
+          image: _buildImage('logo.png'),
+          decoration: pageDecoration,
+        ),
+
+
+      ],
+      onDone: () => _onIntroEnd(context),
+      onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipOrBackFlex: 0,
+      nextFlex: 0,
+      showBackButton: false,
+      //rtl: true, // Display as right-to-left
+      back: const Icon(Icons.arrow_back, color: Colors.white,),
+      skip: const Text('Pular', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+      next: const Icon(Icons.arrow_forward, color: Colors.white,),
+      done: const Text('Feito', style: TextStyle(fontWeight: FontWeight.w600, color: Colors.white)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Colors.white,
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.indigo,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}
